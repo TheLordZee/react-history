1c16ae487110c79b1c92bb3a148bc1baea6cfc97
lordzee <l0rclz33@gmail.com>
2021-10-08 18:40:28 -0400

    finished

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import FuncJokeList from "./JokeList";
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <FuncJokeList />
+      </div>
+    );
+  }
+}
+
+export default App;
